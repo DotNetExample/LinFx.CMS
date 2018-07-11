041f44e0dbb5a85cec68f38e8b7358dc834d7fd4
林松斌 <linsongbin@126.com>
2018-07-11 15:55:51 +0800

    fix

--- src/LinFx.CMS.Core/Properties/AssemblyInfo.cs
+++ src/LinFx.CMS.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LinFx.CMS.Core")]
+[assembly: AssemblyProduct("LinFx.CMS")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
